9b0942b95ee4e29304c0d0785f0e399d6c089b3c
devraj singh <devrajsingh@devrajs-MacBook-Air.local>
2024-12-11 10:45:25 +0530

    adding gtm removed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         gusain Portfolio
+         Devraj Portfolio
         </p>
         <a
           className="App-link"
